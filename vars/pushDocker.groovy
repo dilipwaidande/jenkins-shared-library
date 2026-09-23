356cf48477e5160ffa19e4fa7fd3ad9e02220cae
Dilip Waidande <dilwaid@gmail.com>
2026-09-23 13:14:45 +0530

    P2 Fix variable escaping in Docker login command

--- vars/pushDocker.groovy
+++ vars/pushDocker.groovy
@@ -1,7 +1,7 @@
 def call(String imageName, String buildNumber, String credentialsId) { 
     echo "=== [Shared Library] Pushing Docker Image ===" 
-    withCredentials([usernamePassword(credentialsId: credentialsId, passwordVariable: 'DOCKER\_PASS', usernameVariable: 'DOCKER\_USER')]) { 
-        sh "echo \\$DOCKER\_PASS | docker login -u \\$DOCKER\_USER --password-stdin" 
+    withCredentials([usernamePassword(credentialsId: credentialsId, passwordVariable: 'DOCKER_PASS', usernameVariable: 'DOCKER_USER')]) { 
+        sh "echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin" 
         sh "docker push ${imageName}:${buildNumber}" sh "docker push ${imageName}:latest" 
     } 
 }
