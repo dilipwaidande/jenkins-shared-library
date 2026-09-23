eef76e8c15b0244bcc88e56178f11d8e802c254d
Dilip Waidande <dilwaid@gmail.com>
2026-09-23 13:36:23 +0530

    Fix syntax error in pushDocker.groovy

--- vars/pushDocker.groovy
+++ vars/pushDocker.groovy
@@ -2,6 +2,7 @@ def call(String imageName, String buildNumber, String credentialsId) {
     echo "=== [Shared Library] Pushing Docker Image ===" 
     withCredentials([usernamePassword(credentialsId: credentialsId, passwordVariable: 'DOCKER_PASS', usernameVariable: 'DOCKER_USER')]) { 
         sh "echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin" 
-        sh "docker push ${imageName}:${buildNumber}" sh "docker push ${imageName}:latest" 
+        sh "docker push ${imageName}:${buildNumber}" 
+        sh "docker push ${imageName}:latest" 
     } 
 }
